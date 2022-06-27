9b8fb48bd33db5a02308ffa5c931e19171772067
Eden Landau <edenworky@gmail.com>
2022-06-27 12:06:48 +0300

    clean up

--- script.rb
+++ script.rb
@@ -2,37 +2,28 @@ require 'mini_racer'
 
 context = MiniRacer::Context.new
 context.eval 'let global = globalThis'
-context.eval File.read './system.min.js'
-context.eval File.read './named-register.js'
+context.eval File.read './ruby/system.min.js'
+context.eval File.read './ruby/named-register.js'
 context.eval File.read './dist.js'
 
-def load_index context, &with_index
-  context.attach 'loadedIndex', -> (*a, **kw) { with_index.call *a, **kw }
 
+def load_index context
   context.eval "
     (async function () {
       globalThis.index = await System.import('index')
-      // loadedIndex(index)
     })()
 
-    function getIndex() { return globalThis.index }
+    function getIndex() { return index }
   "
 
-  sleep 0.1
-
-  puts 'after load'
-  index = context.call 'getIndex'
-  with_index.call index
+  context.call 'getIndex'
 end
 
-def make_schemes context, &with_schemes
-  puts 'making...'
-  context.attach 'madeSchemes', -> (*a, **kw) { with_schemes.call *a, **kw }
-  puts 'attached...'
-#   context.eval 'madeSchemes("hello")'
+def make_schemes context
   context.eval "
     globalThis.schemes = tryMakeSchemes()
-    madeSchemes(schemes)
+
+    function getSchemes() { return schemes }
 
     function tryMakeSchemes() {
       try {
@@ -43,27 +34,17 @@ def make_schemes context, &with_schemes
     }
 
     function makeSchemes() {
-      let palette = new index.CorePalette(6770852)
+      let palette = new index.CorePalette(#{6770852})
       return {
         light_scheme: index.Scheme.lightFromCorePalette(palette),
         dark_scheme: index.Scheme.darkFromCorePalette(palette),
       }
     }
   "
-  puts 'after eval'
+
+  context.call 'getSchemes'
 end
 
-load_index context do |index|
-  puts "called with index", index
-  puts context
-  sleep 1
-
-  puts 'making schemes'
-  make_schemes context do |schemes|
-    puts "  schemes:", schemes
-  end
-
-  puts 'after'
-end
-
-
+index = load_index context
+schemes = make_schemes context
+puts "  schemes:", schemes
